376646995efd7011cf2b002433f6a55fe49b046a
JohnTheBeloved <topzy20@gmail.com>
2021-03-24 13:52:08 +0100

    Implement Login Feature and error handing

---
+++ src/main/java/com/airtel/currencyconverter/service/ConversionService.java
@@ -0,0 +1,18 @@
+package com.airtel.currencyconverter.service;
+
+import java.util.List;
+
+import com.airtel.currencyconverter.exception.ResourceNotFoundException;
+import com.airtel.currencyconverter.model.Conversion;
+
+public interface ConversionService {
+
+	Conversion create(Conversion conversion);
+
+	List<Conversion> get();
+
+	void delete(Long id) throws ResourceNotFoundException;
+
+	boolean save(List<Conversion> currencies);
+
+}
